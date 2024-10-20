aba2288edb7232df01ceddd3a9e69aa8c0be160f
Sanju <sanjudilshan554@gmail.com>
2024-10-20 08:42:35 +0530

    changes

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,55 +1,55 @@
 <template>
-    <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
-        <div class="row gx-lg-5 align-items-center  mt-5">
-            <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
-                <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
-                    The best offer <br />
-                    <span style="color: hsl(218, 81%, 75%)">for your business</span>
-                </h1>
-                <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Temporibus, expedita iusto veniam atque, magni tempora mollitia
-                    dolorum consequatur nulla, neque debitis eos reprehenderit quasi
-                    ab ipsum nisi dolorem modi. Quos?
-                </p>
-            </div>
+  <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+      <div class="row gx-lg-5 align-items-center  mt-5">
+          <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+              <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+                  The best offer <br />
+                  <span style="color: hsl(218, 81%, 75%)">for your business</span>
+              </h1>
+              <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Temporibus, expedita iusto veniam atque, magni tempora mollitia
+                  dolorum consequatur nulla, neque debitis eos reprehenderit quasi
+                  ab ipsum nisi dolorem modi. Quos?
+              </p>
+          </div>
 
-            <div class="col-lg-6 mb-5 mb-lg-0 position-relative mt-5 pr-3 mr-0">
-                <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
-                <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
+          <div class="col-lg-6 mb-5 mb-lg-0 position-relative mt-5 pr-3 mr-0">
+              <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
+              <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
 
-                <div class="card bg-glass">
-                    <div class="text-center pt-5">
-                        <h1>Login</h1>
-                    </div>
-                    <div class="card-body px-4 py-5 px-md-5">
-                        <form @submit.prevent="loginUser">
-                            <!-- Email input -->
-                            <div data-mdb-input-init class="form-outline mb-4">
-                                <input type="email" id="form3Example3" class="form-control" v-model="User.email"/>
-                                <label class="form-label" for="form3Example3">Email address</label>
-                            </div>
+              <div class="card bg-glass">
+                  <div class="text-center pt-5">
+                      <h1>Login</h1>
+                  </div>
+                  <div class="card-body px-4 py-5 px-md-5">
+                      <form @submit.prevent="loginUser">
+                          <!-- Email input -->
+                          <div data-mdb-input-init class="form-outline mb-4">
+                              <input type="email" id="form3Example3" class="form-control" v-model="User.email"/>
+                              <label class="form-label" for="form3Example3">Email address</label>
+                          </div>
 
-                            <!-- Password input -->
-                            <div data-mdb-input-init class="form-outline mb-4">
-                                <input type="password" id="form3Example4" class="form-control" v-model="User.password"/>
-                                <label class="form-label" for="form3Example4">Password</label>
-                            </div> 
+                          <!-- Password input -->
+                          <div data-mdb-input-init class="form-outline mb-4">
+                              <input type="password" id="form3Example4" class="form-control" v-model="User.password"/>
+                              <label class="form-label" for="form3Example4">Password</label>
+                          </div> 
 
-                            <!-- Submit button -->
-                            <button type="submit" data-mdb-button-init data-mdb-ripple-init
-                                class="btn btn-primary btn-block mb-4">
-                                Log in
-                            </button> 
-                        </form>
+                          <!-- Submit button -->
+                          <button type="submit" data-mdb-button-init data-mdb-ripple-init
+                              class="btn btn-primary btn-block mb-4">
+                              Log in
+                          </button> 
+                      </form>
 
-                        <div class="text-center">don't have an account <span class="text-primary cursor-pointer"
-                                @click.prevent="visitRegister">register</span></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                      <div class="text-center">don't have an account <span class="text-primary cursor-pointer"
+                              @click.prevent="visitRegister">register</span></div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script setup>
@@ -62,24 +62,24 @@ const router = useRouter();
 const User = ref({});
 
 const visitRegister = () => {
-    router.push({ name: 'register' });
+  router.push({ name: 'register' });
 }
 
 const loginUser = async () => {
-    try {
-        const response = await axios.post('http://127.0.0.1:8000/api/login', User.value);
-        
-        // Store the token in localStorage
-        localStorage.setItem('access_token', response.data.token);
+  try {
+      const response = await axios.post('http://127.0.0.1:8000/api/login', User.value);
+      
+      // Store the token in localStorage
+      localStorage.setItem('access_token', response.data.token);
 
-        // Use the token in future requests
-        axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+      // Use the token in future requests
+      axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
 
-        // Redirect to the dashboard or another page
-        // router.push({ name: 'dashboard' });
-    } catch (error) {
-        console.log('Login error:', error);
-    }
+      // Redirect to the dashboard or another page
+      // router.push({ name: 'dashboard' });
+  } catch (error) {
+      console.log('Login error:', error);
+  }
 };
 
 
@@ -87,42 +87,42 @@ const loginUser = async () => {
 
 <style>
 .background-radial-gradient {
-    background-color: hsl(218, 41%, 15%);
-    background-image: radial-gradient(650px circle at 0% 0%,
-            hsl(218, 41%, 35%) 15%,
-            hsl(218, 41%, 30%) 35%,
-            hsl(218, 41%, 20%) 75%,
-            hsl(218, 41%, 19%) 80%,
-            transparent 100%),
-        radial-gradient(1250px circle at 100% 100%,
-            hsl(218, 41%, 45%) 15%,
-            hsl(218, 41%, 30%) 35%,
-            hsl(218, 41%, 20%) 75%,
-            hsl(218, 41%, 19%) 80%,
-            transparent 100%);
+  background-color: hsl(218, 41%, 15%);
+  background-image: radial-gradient(650px circle at 0% 0%,
+          hsl(218, 41%, 35%) 15%,
+          hsl(218, 41%, 30%) 35%,
+          hsl(218, 41%, 20%) 75%,
+          hsl(218, 41%, 19%) 80%,
+          transparent 100%),
+      radial-gradient(1250px circle at 100% 100%,
+          hsl(218, 41%, 45%) 15%,
+          hsl(218, 41%, 30%) 35%,
+          hsl(218, 41%, 20%) 75%,
+          hsl(218, 41%, 19%) 80%,
+          transparent 100%);
 }
 
 #radius-shape-1 {
-    height: 220px;
-    width: 220px;
-    top: -60px;
-    left: -130px;
-    background: radial-gradient(#44006b, #ad1fff);
-    overflow: hidden;
+  height: 220px;
+  width: 220px;
+  top: -60px;
+  left: -130px;
+  background: radial-gradient(#44006b, #ad1fff);
+  overflow: hidden;
 }
 
 #radius-shape-2 {
-    border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
-    bottom: -60px;
-    right: -110px;
-    width: 300px;
-    height: 300px;
-    background: radial-gradient(#44006b, #ad1fff);
-    overflow: hidden;
+  border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
+  bottom: -60px;
+  right: -110px;
+  width: 300px;
+  height: 300px;
+  background: radial-gradient(#44006b, #ad1fff);
+  overflow: hidden;
 }
 
 .bg-glass {
-    background-color: hsla(0, 0%, 100%, 0.9) !important;
-    backdrop-filter: saturate(200%) blur(25px);
+  background-color: hsla(0, 0%, 100%, 0.9) !important;
+  backdrop-filter: saturate(200%) blur(25px);
 }
 </style>
